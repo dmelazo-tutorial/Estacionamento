914b74ed9ab86a56e76a568736efdd2c4db9ca98
Davi <davi@melazo.net>
2017-05-17 11:48:18 -0300

    Criação do repositório.

---
+++ Estacionamento.Apresentacao/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estacionamento.Apresentacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string placa = textBox1.Text;
+            var client = new ServiceReference.EstacionamentoServiceClient();
+
+            try
+            {
+
+                client.Checkin(placa);
+                client.Close();
+
+                MessageBox.Show(String.Format("Placa '{0}' adicionada.", placa));
+                textBox1.Text = string.Empty;
+
+            }
+            catch (Exception ex)
+            {
+                client.Abort();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string placa = textBox1.Text;
+
+            //try
+            //{
+            //    var valor = Operacoes.Checkout(placa);
+
+            //    MessageBox.Show(String.Format("Placa '{0}' valor de R${1}.", placa, valor));
+            //    textBox1.Text = string.Empty;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+        }
+    }
+}
---
+++ Estacionamento.Servicos/EstacionamentoService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Estacionamento.Negocio;
+
+
+namespace Estacionamento.Servicos
+{
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.PerCall)]
+    public class EstacionamentoService : IEstacionamentoService
+    {
+        public void Checkin(string placa)
+        {
+            Operacoes.Checkin(placa);
+        }
+    }
+}
